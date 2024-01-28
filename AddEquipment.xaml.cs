14e5fa7fd616a5913f78410a656ccd0bd218ac53
silviatag <silviahassantag@yahoo.com>
2024-01-28 23:19:19 +0200

    .

--- AddEquipment.xaml.cs
+++ AddEquipment.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using CareTech.classes;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -30,43 +31,6 @@ namespace CareTech
             connectionString = ConfigurationManager.ConnectionStrings["server=localhost;database=caretech;user=root;password=caretech;"]?.ConnectionString;
         }
 
-        public void SaveEquipment(string equipmentName, string equipmentType, string vendor, decimal acquisitionCost, int expectedLifespan, DateTime maintenanceDate)
-        {
-            String connectionString = ConfigurationManager.ConnectionStrings["server=localhost;database=caretech;user=root;password=caretech;"]?.ConnectionString;
-
-            try
-            {
-                // Create a new SqlConnection using the connection string
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    // Create a SqlCommand to execute the INSERT statement
-                    string query = "INSERT INTO Equipment (EquipmentName, EquipmentType, Vendor, AcquisitionCost, ExpectedLifespan, MaintenanceDate) " +
-                                   "VALUES (@Name, @Type, @Vendor, @Cost, @Lifespan, @Date)";
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        // Add parameters to the command
-                        command.Parameters.AddWithValue("@Name", equipmentName);
-                        command.Parameters.AddWithValue("@Type", equipmentType);
-                        command.Parameters.AddWithValue("@Vendor", vendor);
-                        command.Parameters.AddWithValue("@Cost", acquisitionCost);
-                        command.Parameters.AddWithValue("@Lifespan", expectedLifespan);
-                        command.Parameters.AddWithValue("@Date", maintenanceDate);
-
-                        // Open the connection
-                        connection.Open();
-
-                        // Execute the command
-                        command.ExecuteNonQuery();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                // Handle exceptions (log, show error message, etc.)
-                MessageBox.Show("Error: " + ex.Message);
-            }
-        }
-
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
             // Get the values from the textboxes and datepicker
@@ -76,11 +40,9 @@ namespace CareTech
             decimal acquisitionCost = Convert.ToDecimal(AcquisitionCostTextBox.Text);
             int expectedLifespan = Convert.ToInt32(ExpectedLifespanTextBox.Text);
             DateTime maintenanceDate = MaintenanceDatePicker.SelectedDate.GetValueOrDefault();
-
-            // Call the SaveEquipment method to save the data
-            SaveEquipment(equipmentName, equipmentType, vendor, acquisitionCost, expectedLifespan, maintenanceDate);
-
-            // Optionally, you can display a success message or perform other actions after saving the data
+            Equipment eq = new Equipment(equipmentName,equipmentType,vendor,acquisitionCost,expectedLifespan,maintenanceDate);
+            DB db = new DB();
+            db.InsertEquipment(eq);
             MessageBox.Show("Equipment saved successfully.");
         }
     }
